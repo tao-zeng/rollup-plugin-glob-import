55314bef493c553be55db952811f630f99b76886
Kei Ito <kei.itof@gmail.com>
2017-11-07 00:18:00 +0900

    change test names

--- test/globFiles/index.js
+++ test/globFiles/index.js
@@ -18,19 +18,21 @@ test('globFiles', (test) => {
 			test('finds 2 files', () => {
 				assert.equal(result.size, 2);
 			});
-			test('has a.0.js', () => {
-				assert(result.has(path.join(filesDirectory, 'a.0.js')));
+			const pathToA0 = path.join(filesDirectory, 'a.0.js');
+			test(`has ${pathToA0}`, () => {
+				assert(result.has(pathToA0));
 			});
-			test('has expected a.0.js', () => {
-				const actual = result.get(path.join(filesDirectory, 'a.0.js'));
+			test(`has expected ${pathToA0}`, () => {
+				const actual = result.get(pathToA0);
 				const expected = `${absolutePrefix}/a.0.js`;
 				assert.equal(actual, expected);
 			});
-			test('has a.1.js', () => {
+			const pathToA1 = path.join(filesDirectory, 'a.1.js');
+			test(`has ${pathToA1}`, () => {
 				assert(result.has(path.join(filesDirectory, 'a.1.js')));
 			});
-			test('has expected a.1.js', () => {
-				const actual = result.get(path.join(filesDirectory, 'a.1.js'));
+			test(`has expected ${pathToA1}`, () => {
+				const actual = result.get(pathToA1);
 				const expected = `${absolutePrefix}/a.1.js`;
 				assert.equal(actual, expected);
 			});
@@ -45,19 +47,21 @@ test('globFiles', (test) => {
 			test('finds 2 files', () => {
 				assert.equal(result.size, 2);
 			});
-			test('has a.1.js', () => {
+			const pathToA1 = path.join(filesDirectory, 'a.1.js');
+			test(`has ${pathToA1}`, () => {
 				assert(result.has(path.join(filesDirectory, 'a.1.js')));
 			});
-			test('has expected a.1.js', () => {
-				const actual = result.get(path.join(filesDirectory, 'a.1.js'));
+			test(`has expected ${pathToA1}`, () => {
+				const actual = result.get(pathToA1);
 				const expected = './a.1.js';
 				assert.equal(actual, expected);
 			});
-			test('has b.1.js', () => {
-				assert(result.has(path.join(filesDirectory, 'b.1.js')));
+			const pathToB1 = path.join(filesDirectory, 'b.1.js');
+			test(`has ${pathToB1}`, () => {
+				assert(result.has(pathToB1));
 			});
-			test('has expected b.1.js', () => {
-				const actual = result.get(path.join(filesDirectory, 'b.1.js'));
+			test(`has expected ${pathToB1}`, () => {
+				const actual = result.get(pathToB1);
 				const expected = './b.1.js';
 				assert.equal(actual, expected);
 			});
